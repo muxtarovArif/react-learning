516bd9f54a7ed0d0b2f02ac4a559c91f02ed05ee
Arif Ismail. Muxtarov <arif.muxtarov@aztc.az>
2020-12-21 14:50:30 +0400

    .

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img
+        src="https://www.freelogodesign.org/Content/img/logo-samples/bakary.png"
+        alt=""
+      />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Profile.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <img src="https://via.placeholder.com/125x125" alt=""></img>
+
+      <div>ava+description</div>
+      <div>
+        My Posts
+        <div>New post</div>
+        <div>Post 1</div>
+        <div>Post 2</div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
